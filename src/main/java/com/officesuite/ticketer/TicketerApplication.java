f809806d0756d67885b082ed5192d7b13682afe4
hemanthlucifer <hemanthkumardharanikota29@gmail.com>
2025-06-14 18:15:44 +0530

    initial commit

---
+++ src/main/java/com/officesuite/ticketer/TicketerApplication.java
@@ -0,0 +1,13 @@
+package com.officesuite.ticketer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/officesuite/ticketer/TicketerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.officesuite.ticketer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
